d7a3703368e24767458ac5777748af620721ec63
Dylan <delxhq0@gmail.com>
2017-05-01 16:05:01 +0100

    replace

--- main/java/me/person/delxhq/delxhqsgame/GamePanel.java
+++ main/java/me/person/delxhq/delxhqsgame/GamePanel.java
@@ -3,6 +3,7 @@ package me.person.delxhq.delxhqsgame;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.view.MotionEvent;
@@ -15,9 +16,16 @@ import android.view.SurfaceView;
 
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
+    private Rect r = new Rect();
 
     private RectPlayer player;
     private Point playerPoint;
+    private ObstacleManager obstacleManager;
+
+    private boolean movingPlayer = false;
+
+    private boolean gameOver = false;
+    private long gameOverTime;
 
     public GamePanel(Context context) {
         super(context);
@@ -26,10 +34,21 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
         thread = new MainThread(getHolder(), this);
 
-        player = new RectPlayer(new Rect(100, 100, 200, 200), Color.rgb(255, 0, 0));
-        playerPoint = new Point(150, 150);
+        player = new RectPlayer(new Rect(100, 100, 200, 200), Color.RED);
+        playerPoint = new Point(Constants.SCREEN_WIDTH / 2, 3 * Constants.SCREEN_HEIGHT / 4);
+        player.update(playerPoint);
+
+        obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
 
         setFocusable(true);
+    }
+
+    public void reset() {
+        playerPoint = new Point(Constants.SCREEN_WIDTH / 2, 3 * Constants.SCREEN_HEIGHT / 4);
+        player.update(playerPoint);
+        obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
+        movingPlayer = false;
+
     }
 
     @Override
@@ -63,16 +82,38 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                if (!gameOver && player.getRectangle().contains((int) event.getX(), (int) event.getY()))
+                    movingPlayer = true;
+                if (gameOver && System.currentTimeMillis() - gameOverTime >= 2000) {
+                    reset();
+                    gameOver = false;
+                }
+                break;
+
             case MotionEvent.ACTION_MOVE:
-                playerPoint.set((int) event.getX(), (int) event.getY());
+                if (!gameOver && movingPlayer)
+                    playerPoint.set((int) event.getX(), (int) event.getY());
+                break;
+
+            case MotionEvent.ACTION_UP:
+                movingPlayer = false;
+                break;
         }
 
         return true;
-        //return super.onTouchEvent(event);
+        // return super.onTouchEvent(event);
     }
 
     public void update() {
-        player.update(playerPoint);
+        if (!gameOver) {
+            player.update(playerPoint);
+            obstacleManager.update();
+
+            if (obstacleManager.playerCollide(player)) {
+                gameOver = true;
+                gameOverTime = System.currentTimeMillis();
+            }
+        }
     }
 
     @Override
@@ -82,5 +123,24 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
         canvas.drawColor(Color.WHITE);
 
         player.draw(canvas);
+        obstacleManager.draw(canvas);
+
+        if (gameOver) {
+            Paint paint = new Paint();
+            paint.setTextSize(85);
+            paint.setColor(Color.RED);
+            drawCenterText(canvas, paint, "Game Over");
+        }
+    }
+
+    private void drawCenterText(Canvas canvas, Paint paint, String text) {
+        paint.setTextAlign(Paint.Align.LEFT);
+        canvas.getClipBounds(r);
+        int cHeight = r.height();
+        int cWidth = r.width();
+        paint.getTextBounds(text, 0, text.length(), r);
+        float x = cWidth / 2f - r.width() / 2f - r.left;
+        float y = cHeight / 2f + r.height() / 2f - r.bottom;
+        canvas.drawText(text, x, y, paint);;
     }
 }
