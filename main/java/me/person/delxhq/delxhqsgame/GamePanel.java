5c8f6b6bdaa9ff2a755dbc57b8e95aabbc0843a8
Dylan <delxhq0@gmail.com>
2017-05-01 02:41:46 +0100

    Red man

--- main/java/me/person/delxhq/delxhqsgame/GamePanel.java
+++ main/java/me/person/delxhq/delxhqsgame/GamePanel.java
@@ -2,6 +2,9 @@ package me.person.delxhq.delxhqsgame;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -13,12 +16,18 @@ import android.view.SurfaceView;
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
 
+    private RectPlayer player;
+    private Point playerPoint;
+
     public GamePanel(Context context) {
         super(context);
 
         getHolder().addCallback(this);
 
         thread = new MainThread(getHolder(), this);
+
+        player = new RectPlayer(new Rect(100, 100, 200, 200), Color.rgb(255, 0, 0));
+        playerPoint = new Point(150, 150);
 
         setFocusable(true);
     }
@@ -43,22 +52,35 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
             try {
                 thread.setRunning(true);
                 thread.start();
-            } catch (Exception e) {e.printStackTrace();}
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             retry = false;
         }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                playerPoint.set((int) event.getX(), (int) event.getY());
+        }
+
+        return true;
+        //return super.onTouchEvent(event);
     }
 
     public void update() {
-
+        player.update(playerPoint);
     }
 
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
+
+        canvas.drawColor(Color.WHITE);
+
+        player.draw(canvas);
     }
 }
