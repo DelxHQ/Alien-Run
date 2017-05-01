d4ef7c093468db86229d7cee53a8d91772a1f530
Dylan <delxhq0@gmail.com>
2017-05-01 15:59:15 +0100

    Create Constants.java

---
+++ main/java/me/person/delxhq/delxhqsgame/Constants.java
@@ -0,0 +1,10 @@
+package me.person.delxhq.delxhqsgame;
+
+/**
+ * Created by delxh on 01/05/2017.
+ */
+
+public class Constants {
+    public static int SCREEN_WIDTH;
+    public static int SCREEN_HEIGHT;
+}
